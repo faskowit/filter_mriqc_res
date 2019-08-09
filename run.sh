c7b41d3ce4a8649aba32d45ebeb15cac54579eff
faskowit <jfaskowi@iu.edu>
2019-08-09 11:54:39 -0400

    added help text to run; make odir necessary

--- run.sh
+++ run.sh
@@ -14,9 +14,22 @@ function checkisfile {
 EXEDIR=$(dirname "$(readlink -f "$0")")/
 funcThrP=25
 
+function help_prompt {
+cat <<helptext
+	USAGE ${0} 
+		-f 		path to mriqc's group_bold.tsv
+		-a 		path to mriqc's group_T1w.tsv
+		-t 		threshold percent num of fd outliers (def 25; as in 25 percent)
+		-o 		path to output directory
+
+		need to minimally provide a tsv and output dir
+
+helptext
+}
+
 if [[ $# -lt 2 ]] ; then
-	echo "need at least two args"
-	exit
+	help_prompt
+	exit 1
 fi
 
 # read in files
@@ -39,11 +52,16 @@ while [ "$1" != "" ]; do
         -o | -out ) shift
 								oDir=$1
 								;;
-        * )                     echo "see script"
+        * )                     help_prompt
                                 exit 1
     esac
     shift #this shift "moves up" the arg in after each case
 done
+
+if [[ -z ${oDir} ]] ; then
+	echo "need provide output dir"
+	exit 1
+fi
 
 # make the output dir
 mkdir -p $oDir
